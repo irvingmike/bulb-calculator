9d8113fb2ccb4ab0e0eec5c70d95bcdad44a6ae3
Aaron Anderson <aanderso@amfam.com>
2016-06-29 08:47:30 -0500

    Initial layout complete

---
+++ app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { bootstrap } from "@angular/platform-browser-dynamic";
+
+@Component ({
+  selector: 'lightbulb-app',
+  templateUrl: 'app/LightbulbApp.html',
+})
+
+class LightbulbApp {}
+
+bootstrap(LightbulbApp);
